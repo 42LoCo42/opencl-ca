3a56e8c684dae2e492ac6124054494a756dd7406
Leon Schumacher <leonsch@protonmail.com>
2025-04-30 08:20:48 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  outputs = { flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; }; in rec {
+        packages.default = { };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ packages.default ];
+          packages = with pkgs; [
+            gcc
+            gnumake
+            ocl-icd
+            opencl-headers
+            readline
+          ];
+        };
+      });
+}
